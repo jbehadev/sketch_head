2485ef2c6c5791d0b3e9322578d20b722f6731df
Jeremy Beha <jbeha@chariotsolutions.com>
2023-06-06 23:47:21 -0400

    Cleaned up Nicegui work and added cycle limiter to c code

--- heads_up.py
+++ heads_up.py
@@ -1,34 +1,34 @@
 import serial
 import time
+import sys
+from nicegui import ui
+
 
 arduino = serial.Serial(port='/dev/tty.usbmodem141101', baudrate=9600, timeout=1)
 def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
+    if type(x) == int:
+        arduino.write(x.to_bytes(1, sys.byteorder))
+    else:
+        arduino.write(bytes(x, 'utf-8'))
    
-print(arduino.readline())
-"""
-while True:
-    command = input("What?:")
-    chunked = command.split(' ')
-    print(chunked)
-    write_read(chunked[0])
-    write_read(ascii(int(chunked[1])))
-    write_read('|')
-    write_read('D')
-    write_read(ascii(int(chunked[2])))
-    write_read('|')
-    write_read('E')
-    time.sleep(1)
-    while True:
-        line = arduino.readline()
-        if line == b'':
-            break
-        else:
-            print(line)
-"""
-from nicegui import ui
+
 
 def createEvent():
+    # left eye color
+    left_eye_color = tuple(int(left_color_picker.value.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    write_read('L')
+    write_read(left_brightness_slider.value)
+    write_read(left_eye_color[0])
+    write_read(left_eye_color[1])
+    write_read(left_eye_color[2])
+    write_read('|')
+    right_eye_color = tuple(int(right_color_picker.value.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+    write_read('R')
+    write_read(right_brightness_slider.value)
+    write_read(right_eye_color[0])
+    write_read(right_eye_color[1])
+    write_read(right_eye_color[2])
+    write_read('|')
     write_read('T')
     write_read(ascii(tilt_slider.value))
     write_read('|')
@@ -36,19 +36,52 @@ def createEvent():
     write_read(ascii(swivel_slider.value))
     write_read('|')
     write_read('D')
-    write_read(ascii(duraction_slider.value))
+    write_read(ascii(duration_slider.value))
     write_read('|')
     write_read('E')
+    while True:
+        line = arduino.readline()
+        if line == b'':
+            break
+        else:
+            log.push(line.decode().strip('\r\n'))
+    
 
 ui.label('Control Fred')
-tilt_slider = ui.slider(min=1, max=220, value=100)
-swivel_slider = ui.slider(min=1, max=180, value=90)
-duraction_slider = ui.slider(min=50, max=300, value=150)
 
-ui.label().bind_text_from(tilt_slider, 'value')
+ui.label('Tilt')
+with ui.grid(columns=2).classes('w-full'):
+    tilt_slider = ui.slider(min=1, max=220, value=100)
+    ui.label().bind_text_from(tilt_slider, 'value')
 
-ui.button('BUTTON', on_click=createEvent)
+ui.label('Swivel')
+with ui.grid(columns=2).classes('w-full'):
+    swivel_slider = ui.slider(min=1, max=180, value=90)
+    ui.label().bind_text_from(swivel_slider, 'value')
 
+with ui.grid(columns=2).classes('w-full'):
+    ui.label('Left Eye')
+    ui.label('Right Eye')
+with ui.grid(columns=6).classes('w-full'):
+    left_color_picker = ui.color_input(label='Color', value='#ff0000')
+
+    left_brightness_slider = ui.slider(min=0, max=255, value=150)
+    ui.label().bind_text_from(left_brightness_slider, 'value')
+
+    right_color_picker = ui.color_input(label='Color', value='#ff0000')
+
+    right_brightness_slider = ui.slider(min=0, max=255, value=150)
+    ui.label().bind_text_from(right_brightness_slider, 'value')
+
+with ui.grid(columns=2).classes('w-full'):
+    duration_slider = ui.radio({400: 'Slow', 150: 'Medium', 50: 'Fast'},value=150).props('inline')
+    ui.label().bind_text_from(duration_slider, 'value')
+
+ui.button('Instruct!', on_click=createEvent)
+
+log = ui.log(max_lines=1000).classes('w-full h-20')
+
+log.push(arduino.readline().decode())
 ui.run()
 
     
